8971aecd112d0ee9715bbca03d58790f3a8a826d
Vivan Kumar <vivan2003.vk@gmail.com>
2025-09-04 21:43:30 +0530

    express server setup done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 const express = require("express");
+const { PORT } = require("../src/config/server-config");
+const bodyparser = require("body-parser");
+
+const app = express();
+
+app.get("/ping", (req, res) => {
+    return res.json({
+        message: "problem service is alive"
+    })
+});
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.text());
+
+app.listen(PORT, () => {
+    console.log(`server started at port: ${PORT}`);
+})
